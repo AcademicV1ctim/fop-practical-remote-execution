8abf62f4df3396ee2b65488f2fd0ae202b6bd12d
AcademicV1ctim <nathaniel0911@gmail.com>
2025-08-29 02:18:52 +0800

    eslint config added

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+import jsonPlugin from "@eslint/json";
+import cssPlugin from "@eslint/css";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: [".vscode/**", "node_modules/**", "package-lock.json", "package.json" , "pgdb.js", "app.js", "evaluate.test.js", "judge0"],
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      js,
+    },
+    rules: js.configs.recommended.rules,
+  },
+
+  {
+    files: ["**/*.json"],
+    ...jsonPlugin.configs.recommended,
+  },
+
+  {
+    files: ["**/*.css"],
+    ...cssPlugin.configs.recommended,
+  },
+]);
